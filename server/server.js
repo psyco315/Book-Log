9eef351512969dc1fb5a2be25d8f2831f192313b
psyco315 <parth2004batman@gmail.com>
2025-08-24 18:47:59 +0530

    Deploying under-development app

--- server/server.js
+++ server/server.js
@@ -28,6 +28,10 @@ app.use((err, req, res, next) => {
     });
 });
 
+app.get('/test', (req, res) => {
+    res.send('Hello from Express!');
+});
+
 app.get('/api/test', (req, res) => {
     res.send('Hello from Express!');
 });
